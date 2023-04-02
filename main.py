b72b5d7045d06a448a839bf33cf7b472056a3bad
akds-singh <akds.singh@google.com>
2023-04-02 13:04:37 +0530

    learning Grind in TkInter

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-import pandas
+import tkinter
+
+window = tkinter.Tk()
+window.title("my GUI app")
+window.minsize(width=500, height=300)
+
+# Label
+print(type(window))
+my_label = tkinter.Label(text='hello world', font=('Arial', 24, 'bold'))
+my_label.pack()
+
+
+# my_label['text'] = 'my name  is akash'
+
+
+# Button
+def button_clicked():
+    print(text.get())
+    my_label['text'] = text.get()
+
+
+my_button = tkinter.Button(text='click', command=button_clicked)
+my_button.pack()
+
+# Input text
+
+text = tkinter.Entry(window, width=10)
+text.pack()
+
+my_input_text = text.get()
+
+print(text.get())
+window.mainloop()
